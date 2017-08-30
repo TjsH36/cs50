aaec6d8bc67523cc3f913ea3696d51cd23197bfd
TjsH36 <TjsH36@users.noreply.github.com>
2017-08-30 15:37:03 +0000

    20170830T153558Z

---
+++ Dice/DiceUtilities/TestSetUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace Dice.DiceUtilities
+{
+    public class TestSetUp
+    {
+        public static IWebDriver driver;
+        public static String et = ConfigurationManager.AppSettings["ExecutionType"];
+        public static String browser = ConfigurationManager.AppSettings["Browser"];
+        public static IWebDriver BringMyDriver()
+        {
+            if (et.Equals("Local"))
+            {
+                return BringLocalDriver();
+            }
+            else if (et.Equals("Remote"))
+            {
+                return BringRemoteDriver();
+            }
+            else
+            {
+                return BringLocalDriver();
+            }
+        }
+
+        public static IWebDriver BringLocalDriver()
+        {
+            if (browser.Equals("ie"))
+            {
+                driver = new InternetExplorerDriver();
+                return driver;
+            }
+            else if (browser.Equals("chrome"))
+            {
+                driver = new ChromeDriver();
+                return driver;
+            }
+            else
+            {
+                driver = new FirefoxDriver();
+                return driver;
+            }
+        }
+
+        public static IWebDriver BringRemoteDriver()
+        {
+            return new FirefoxDriver();
+
+        }
+    }
+}
